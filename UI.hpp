21496b1465464b7f0aeda6d17cf3812c05727f10
Pratap-kunwar <141573509+Pratap-kunwar@users.noreply.github.com>
2024-08-02 20:22:55 +0545

    Update UI.hpp

--- UI.hpp
+++ UI.hpp
@@ -1,38 +1,33 @@
+UI.hpp
 #ifndef UI_HPP
 #define UI_HPP
 
 #include <SFML/Graphics.hpp>
+#include <string>
+
 using namespace sf;
 
 class UI : public Drawable {
+private:
+    Font font;
+    Text tetrisText, scoreTextTitle, maxScoreTextTitle, scoreText, maxScoreText, byText, gameOverText, newScoreText;
+    Text playText, exitText, highScoreText;
+    bool gameOver, newScore, showHighScores;
+    Clock clock;
+
+    virtual void draw(RenderTarget& rt, RenderStates rs) const;
+
 public:
     UI();
     void SetScore(int x);
     void SetMaxScore(int x);
     void GameOver();
     void NewScore();
-    void ResetGameOverFlags(); // Method declaration for resetting flags
+    void ResetGameOverFlags();
     bool AskToPlayAgain(RenderWindow& window);
     bool ShowMainMenu(RenderWindow& window);
-
-private:
-    Font font;
-    Text scoreTextTitle;
-    Text maxScoreTextTitle;
-    Text scoreText;
-    Text maxScoreText;
-    Text byText;
-    Text gameOverText;
-    Text newScoreText;
-    Text playText;
-    Text exitText;
-    Text tetrisText; // New text for "Tetris"
-
-    bool gameOver = false;
-    bool newScore = false;
-    Clock clock;
-
-    virtual void draw(RenderTarget& rt, RenderStates rs) const override;
+    void ShowHighScores(RenderWindow& window);
+    std::string GetPlayerName(RenderWindow& window);
 };
 
 #endif // UI_HPP
